fcbd503eb6d34ce6cd69858648044233a2b2793f
Toni Nguyen <d3vzero@outlook.de>
2024-11-07 18:49:01 +0100

    cpp: Add formating plugin

--- cpp/default.nix
+++ cpp/default.nix
@@ -6,6 +6,14 @@
   plugins = {
     clangd-extensions.enable = true;
 
+    conform-nvim = {
+      enable = true;
+
+      settings = {
+        formatters_by_ft.cpp = [ "clang_format" ];
+      };
+    };
+
     lsp.servers.clangd = {
       enable = true;
       package = null;
