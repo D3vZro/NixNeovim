da7e0c61e9a5a529806cfa4dd3a390932daa1b92
Toni Nguyen <zero@d3vzero.de>
2026-05-09 22:52:54 +0200

    lean: Add Lake restart keybind

--- lean/default.nix
+++ lean/default.nix
@@ -3,6 +3,13 @@
     ../base/base.nix
   ];
 
+  lsp.keymaps = [
+    {
+      key = "<leader>R";
+      action = "require('lean.lsp').restart_file()";
+    }
+  ];
+
   plugins.lean = {
     enable = true;
   };
